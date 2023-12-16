4fe723f043348518604c6d69cdd463ad4243a233
Meghana <meghanamanchal@gmail.com>
2023-12-16 18:16:13 +0530

    game

--- games.js
+++ games.js
@@ -4,19 +4,31 @@ const roundValue = document.getElementById("roundValue");
 const playGameBtn = document.getElementById("playGame");
 const resultMessage = document.getElementById("result");
 const backCard = document.querySelectorAll(".backCard");
+const timerDisplay = document.getElementById("timer");
 
 let score = 0;
 let round = 0;
 let correctCardIndex;
+let timer=3;
 
 const cardImages = [
   "/images/ace.png",
-
-  "/images/jack.png",
-
   "/images/king.png",
-
+  "/images/jack.png",
   "/images/queen.png",
+  "/images/king.png",
+  "/images/jack.png",
+  "/images/queen.png",
+  "/images/king.png",
+  "/images/jack.png",
+  "/images/queen.png",
+  "/images/king.png",
+  "/images/jack.png",
+  "/images/queen.png",
+  "/images/king.png",
+  "/images/jack.png",
+  "/images/queen.png",
+ 
 ];
 
 var audio = new Audio("sound/Sakura-Girl-Daisy-chosic.com_.mp3");
@@ -28,10 +40,10 @@ playGameBtn.addEventListener("click", function () {
 setCorrectCard();
   increaseRound();
   shuffleCards();
+  countDown();
   playGameBtn.remove();
-  startGameMessage.style.display = "block";
+  startGameMessage.style.display = "block"; 
 });
-
 function increaseRound() {
   round++;
   roundValue.textContent = round;
@@ -39,25 +51,19 @@ function increaseRound() {
 function increaseScore() {
   score += 10;
   scoreValue.textContent = score;
-
-  //let scoreValue = score;
-
-  localStorage.setItem("scoreValue", score);
+localStorage.setItem("scoreValue", score);
+countDown();
 }
 
 function shuffleCards() {
   const container = document.querySelector(".card-container");
   const cards = Array.from(container.children);
-  //   console.log(cards);
+ 
   for (let i = cards.length - 1; i > 0; i--) {
     const randomIndex = Math.floor(Math.random() * (i + 1));
     container.insertBefore(cards[randomIndex], cards[i].nextElementSibling);
   }
-
-  // increaseRound();
-  //increaseScore();
 }
-
 function createCardWithImage(imagePath,index) {
   const card = document.createElement("div");
   card.classList.add("card");
@@ -67,52 +73,50 @@ function createCardWithImage(imagePath,index) {
   card.style.backgroundSize = "contain";
   card.style.backgroundRepeat = "no-repeat";
 
-  card.innerHTML = `<div class="backCard" ></div>`;
-
   card.addEventListener("click", function () {
+    timer=3;
     if (imagePath === "/images/ace.png") {
       resultMessage.textContent = "Hurray..!!.You won!🥳";
-    // backCard.style.opacity = "0";
+    
     setTimeout(() => {
     card.style.backgroundImage = `url(${imagePath})`;
-  }, 2000);
+  }, 1000);
       shuffleCards();
       increaseRound();
       increaseScore();
+      countDown();
       setTimeout(() => {
         resultMessage.textContent = "";
-       // backCard.style.opacity = "0";
-       card.style.backgroundImage = `url('')`;
-       revealAllCards(); 
       }, 2000);
     } else {
       resultMessage.textContent = "Oops! You lost! 😞"; 
-      revealAllCards();
       setTimeout(() => {
       localStorage.setItem("scoreValue", score);
       window.location.href = "./gameover.html";
-    }, 2000); 
+       }, 500); 
     }
   });
   return card;
 }
-function revealAllCards() {
-  const cards = document.querySelectorAll('.card');
-  cards.forEach(card => {
-    setTimeout(() => {
-      card.style.backgroundImage = `url(${cardImages[card.dataset.index]})`; 
-    }, 2000); 
-  });
-}
 function createAndAppendCardsWithImages() {
-  cardImages.forEach((imagePath) => {
-    const card = createCardWithImage(imagePath);
-
-    cardContainer.appendChild(card);
+  cardImages.forEach((imagePath,index) => {
+    const card = createCardWithImage(imagePath,index);
+   cardContainer.appendChild(card);
   });
 }
 function setCorrectCard() {
   correctCardIndex = Math.floor(Math.random() * cardImages.length);
 }
+function countDown(){
+  localStorage.setItem("scoreValue",score);
+  if (timer==0){
+    window.location.href = "./gameover.html";
+}
+timerDisplay.innerText=timer;
+timer--;
+
+};
+setInterval(countDown,1000)
+
 
 
